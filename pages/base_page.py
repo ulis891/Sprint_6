f3ab7e800f73c90d3c9063ff28bcf71bf89aaaf3
Aleksandr Kostromin <ulis89@yandex.ru>
2025-08-28 23:05:15 +0300

    add base_page.py

---
+++ pages/base_page.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.base_url = "https://qa-scooter.praktikum-services.ru/"
+
+    def go_to_site(self):
+        return self.driver.get(self.base_url)
+
+    def find_element(self, locator, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+
+    def find_elements(self, locator, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
+
+    def cllick_element(self, locator, timeout=10):
+        element = self.find_element(locator, timeout)
+        element.click()
+
+    def get_current_url(self):
+        return self.driver.current_url
+
+
