d2577b80e28f8f4bea9de7738e72a8ebd8de1150
Aleksandr Kostromin <ulis89@yandex.ru>
2025-08-28 22:50:50 +0300

    add conftest.py

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+
+@pytest.fixture(scope="function")
+def driver():
+    options = Options()
+    #options.add_argument('--headless')
+    options.add_argument('--window-size=1920,1080')
+    driver = webdriver.Firefox(options=options)
+    yield driver
+    driver.quit()
