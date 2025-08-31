71bf1e3288d2050600682b09868863325a9f9fff
Aleksandr Kostromin <ulis89@yandex.ru>
2025-08-31 22:06:58 +0300

    add methods to base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -24,14 +24,32 @@ class BasePage:
     def get_current_url(self):
         return self.driver.current_url
 
-    def wait_for_element_to_be_visible(self, locator, time=10):
-        return WebDriverWait(self.driver, time).until(
+    def wait_for_element_to_be_visible(self, locator, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(
             EC.visibility_of_element_located(locator),
             message=f"Element {locator} is not visible"
         )
 
-    def wait_for_element_to_be_clickable(self, locator, time=10):
-        return WebDriverWait(self.driver, time).until(
+    def wait_for_element_to_be_clickable(self, locator, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(
             EC.element_to_be_clickable(locator),
             message=f"Element {locator} is not clickable"
         )
+
+    def wait_for_page_load(self, timeout=15):
+        WebDriverWait(self.driver, timeout).until(
+            lambda driver: driver.execute_script("return document.readyState") == "complete")
+
+    def wait_for_new_tab(self, timeout=10):
+        current_tabs_count = len(self.driver.window_handles)
+        WebDriverWait(self.driver, timeout).until(
+            lambda driver: len(driver.window_handles) > current_tabs_count
+        )
+
+    def switch_to_new_tab(self):
+        original_tab = self.driver.current_window_handle
+        for tab_handle in self.driver.window_handles:
+            if tab_handle != original_tab:
+                self.driver.switch_to.window(tab_handle)
+                return tab_handle
+        return None
