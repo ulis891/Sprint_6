3a3616f95c9bfada09e2b99e5cbde6fe024e0c80
Aleksandr Kostromin <ulis89@yandex.ru>
2025-09-01 19:00:42 +0300

    fix conftest.py

--- conftest.py
+++ conftest.py
@@ -6,7 +6,6 @@ from selenium.webdriver.firefox.options import Options
 @pytest.fixture(scope="function")
 def driver():
     options = Options()
-    #options.add_argument('--headless')
     options.add_argument('--window-size=1920,1080')
     driver = webdriver.Firefox(options=options)
     yield driver
