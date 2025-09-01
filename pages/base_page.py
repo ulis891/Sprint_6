6825481626bc74c678b87450e40d813a5cca266c
Aleksandr Kostromin <ulis89@yandex.ru>
2025-09-01 18:15:12 +0300

    fix import

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
 
 
 class BasePage:
